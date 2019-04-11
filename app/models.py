aeb1a616203c2b49abceafeca90c741107c0bb49
Mihika <mohta.mihika@gmail.com>
2019-04-11 08:59:48 +0530

    Get and create card implemented

--- app/models.py
+++ app/models.py
@@ -1,22 +1,60 @@
-"""from app import db
+from app import db
+from flask import url_for
+
+
+class PaginatedAPIMixin(object):
+	@staticmethod
+	def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+		resources = query.paginate(page, per_page, False)
+		data = {
+			'items': [item.to_dict() for item in resources.items],
+			'_meta': {
+				'page': page,
+				'per_page': per_page,
+				'total_pages': resources.pages,
+				'total_items': resources.total
+			},
+			'_links': {
+				'self': url_for(endpoint, page=page, per_page=per_page, **kwargs),
+				'next': url_for(endpoint, page=page+1, per_page=per_page, **kwargs) if resources.has_next else None,
+				'prev': url_for(endpoint, page=page-1, per_page=per_page, **kwargs) if resources.has_prev else None
+			}
+		}
+
+		return data
 
 
 class Board(db.Model):
 	id = db.Column(db.String(10), primary_key=True)
 	title = db.Column(db.String(64))
 	visibility = db.Column(db.String(32), index=True)
-	cards = db.relationship('Card', backref='board', cascade='all,delete', lazy='dynamic')
-
-
-class Card(db.Model):
-	id = db.Column(db.String(10), primary_key=True)
-	title = db.Column(db.String(64))
-	board_id = db.Column(db.String(10), db.ForeignKey('board.id'))
-	items = db.relationship('List', backref='card', cascade='all,delete', lazy='dynamic')
+	cards = db.relationship('List', backref='board', cascade='all,delete', lazy='dynamic')
 
 
 class List(db.Model):
 	id = db.Column(db.String(10), primary_key=True)
 	title = db.Column(db.String(64))
+	board_id = db.Column(db.String(10), db.ForeignKey('board.id'))
+	items = db.relationship('Card', backref='list', cascade='all,delete', lazy='dynamic')
+
+
+class Card(PaginatedAPIMixin, db.Model):
+	id = db.Column(db.String(10), primary_key=True)
+	title = db.Column(db.String(64))
 	description = db.Column(db.String(250))
-	card_id = db.Column(db.String(10), db.ForeignKey('card.id'))"""
+	list_id = db.Column(db.String(10), db.ForeignKey('list.id'))
+
+	def to_dict(self):
+		data = {
+			'id': self.id,
+			'title': self.title,
+			'description': self.title,
+			'list_id': self.list_id
+		}
+		return data
+
+	def from_dict(self, cid, data):
+		setattr(self, 'id', cid)
+		for field in ['title', 'description', 'list_id']:
+			if field in data:
+				setattr(self, field, data[field])
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
-from app import app
+from app import app, db
+from flask import jsonify, request, url_for
 import random
 import string
+from app.models import Card
+import uuid
 
 
 def generate_random_string():
@@ -8,6 +11,38 @@ def generate_random_string():
 	return rand
 
 
-@app.route('/')
-def index():  # Login/ Registration
-	return "Hello, World"
+@app.route('/trillo/cards/<id>', methods=['GET'])
+def get_card(id):
+	# return 'Hello, World'
+	return jsonify(Card.query.get_or_404(id).to_dict())
+
+
+@app.route('/trillo/cards', methods=['GET'])
+def get_cards():
+	page = request.args.get('page', 1, type=int)
+	per_page = min(request.args.get('per_page', 100, type=int), 100)
+	data = Card.to_collection_dict(Card.query, page, per_page, 'get_cards')
+	return jsonify(data)
+	pass
+
+
+@app.route('/trillo/cards', methods=['POST'])
+def create_card():
+	data = request.get_json() or {}
+	if 'title' not in data or 'list_id' not in data:
+		return 'Bad Request: Must include title of the card'
+	cid = str(uuid.uuid1())
+	cid = cid[0:9]
+	card = Card()
+	card.from_dict(cid, data)
+	db.session.add(card)
+	db.session.commit()
+	response = jsonify(card.to_dict())
+	response.status_code = 201
+	response.headers['Location'] = url_for('get_card', id=card.id)
+	return response
+
+
+@app.route('/trillo/card/<id>', methods=['PUT'])
+def update_card(id):
+	pass
