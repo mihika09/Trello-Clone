8adfd0b210ead0ece46f73179b32d2f565d170fc
Mihika <mohta.mihika@gmail.com>
2019-04-23 01:23:52 +0530

    Moved the table creation queries to a separate file

--- app/__init__.py
+++ app/__init__.py
@@ -14,74 +14,7 @@ app.register_blueprint(lists_bp)
 from app.boards import bp as boards_bp
 app.register_blueprint(boards_bp)
 
-from app import routes, dbs
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
 
-
-# ---------------------------------------------------------------------------------------------------------
-
-"""
-// Creation of DB
-
-dbname = 'trillodb'
-
-con = psycopg2.connect(dbname='postgres', user='mallikamohta', host='localhost', port=5432)
-cur = con.cursor()
-cur.execute("select exists(SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('dbname'))")
-result = cur.fetchall()
-print(result[0][0])
-"""
-
-# ---------------------------------------------------------------------------------------------------------
-
-res = dbs.Database().run_query("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME='board'")
-
-if res:
-	print("Table exists")
-
-if not res:
-	query = "create table board (" \
-			"id VARCHAR(36) PRIMARY KEY," \
-			"title VARCHAR(100)," \
-			"visibility VARCHAR(20) CHECK (visibility = 'public' or visibility = 'private') DEFAULT 'public');"
-	dbs.Database().run_query(query)
-	print("----------------------------------------------------")
-	print("Board created")
-	"""query = "INSERT INTO board VALUES('1', 'test_board');"
-	dbs.Database().run_query(query)
-	print("----------------------------------------------------")"""
-
-# ---------------------------------------------------------------------------------------------------------
-
-res = dbs.Database().run_query("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME='list'")
-
-if res:
-	print("Table exists")
-
-if not res:
-	query = "create table list (" \
-			"id VARCHAR(36) PRIMARY KEY," \
-			"title VARCHAR(100)," \
-			"board_id VARCHAR(36)," \
-			"FOREIGN KEY (board_id) REFERENCES board(id) ON DELETE CASCADE)"
-	dbs.Database().run_query(query)
-	print("----------------------------------------------------")
-	print("List created")
-
-# ---------------------------------------------------------------------------------------------------------
-
-res = dbs.Database().run_query("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME='card'")
-
-if res:
-	print("Table exists")
-
-if not res:
-	query = "create table card (" \
-			"id VARCHAR(36) PRIMARY KEY," \
-			"title VARCHAR(100)," \
-			"description TEXT," \
-			"list_id VARCHAR(36)," \
-			"FOREIGN KEY (list_id) REFERENCES list(id) ON DELETE CASCADE) "
-	dbs.Database().run_query(query)
-	print("----------------------------------------------------")
-	print("Card created")
-	print("----------------------------------------------------")
+from app import create_db, routes, dbs
